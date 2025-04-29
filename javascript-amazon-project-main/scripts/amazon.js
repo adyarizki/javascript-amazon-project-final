f71965af4fe7bf6d0b2da4cdf49453cb18387518
adyarizki <adyaprilriyanrizki@gmail.com>
2025-04-29 08:23:20 +0700

    exercises 14 add input update and event keydown and alert 1-999

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurency} from "./utils/money.js";
 
@@ -66,16 +66,17 @@ products.forEach((products) => {
 
     
 
-    function uppdateCartQuantity() {
-      let cartQuantity = 0;
-                
-        cart.forEach((cartItem) => {
-            cartQuantity += cartItem.quantity;
-        });
+function uppdateCartQuantity() {
+       
+      const cartQuantity = calculateCartQuantity();
 
-        document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+
+      
     }
+    
+  uppdateCartQuantity();
 
     document.querySelectorAll('.js-add-to-cart')
         .forEach((button) => {

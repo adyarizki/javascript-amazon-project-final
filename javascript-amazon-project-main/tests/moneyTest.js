1e3aed402155a9cd88125eb56ce79360fb6aa808
adyarizki <adyaprilriyanrizki@gmail.com>
2025-05-03 18:05:20 +0700

    create automated tests for formatCurrency

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,32 @@
+ import {formatCurency} from '../scripts/utils/money.js';
+
+console.log('test suite : formatCurrency');
+
+
+ console.log('converts cents into dollars');
+ 
+
+if (formatCurency(2095) === '20.95') {
+    console.log('passed');   
+} else {
+    console.log('failed');
+    
+}
+
+console.log('works with 0');
+
+if (formatCurency(0) === '0.00') {
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+console.log('round up to the nearest cent');
+
+
+if (formatCurency(2000.5) === '20.01') {
+    console.log('passed');
+}else{
+    console.log('failed');
+    
+}

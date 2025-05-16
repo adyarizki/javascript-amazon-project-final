108ff41d217b2974adb151858c4b37fedaa12caa
adyarizki <adyaprilriyanrizki@gmail.com>
2025-05-16 19:12:12 +0700

    convert loadProducts from xhr fetch

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -105,6 +105,30 @@ object3.method();
 */
 export let products = [];
 
+export function loadProductsFetch(params) {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products =productsData.map((productDetail) => {
+      if (productDetail.type === 'clothing') {
+        return new Clothing(productDetail);
+      }else if (productDetail.type === 'appliance') {
+        return new Appliance(productDetail);
+      }
+      return new Product(productDetail);
+    });
+    console.log('load products');
+  });
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+  
+});
+*/
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
